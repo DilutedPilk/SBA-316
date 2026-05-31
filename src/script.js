aeccfd88b6b42a96035e55ba2410da458de37bda
dilutedpilk <myawesome4@gmail.com>
2026-05-31 18:37:38 -0400

    completed project, no errors

--- src/script.js
+++ src/script.js
@@ -30,6 +30,13 @@ dropdown.style.padding = "5px";
 dropdown.style.top = "0"
 dropdown.style.right = "0"
 
+dropdown.lastElementChild.style.position = "fixed"
+dropdown.lastElementChild.style.height = "30px";
+dropdown.lastElementChild.style.width = "210px";
+dropdown.lastElementChild.style.padding = "5px";
+dropdown.lastElementChild.style.top = "0"
+dropdown.lastElementChild.style.right = "0"
+
 // ----------------------------------------------->> Main content
 
 const main = document.querySelector('main');
@@ -56,13 +63,31 @@ content.appendChild(title);
 content.appendChild(document.createElement('hr'));
 
 const changeDiv = document.createElement('div')
-content.appendChild(changeDiv)
+changeDiv.textContent = "Please log in to gain access. (Hint: program has the username and password logged)"
+changeDiv.style.textAlign = 'center';
+content.appendChild(changeDiv);
+
+const gif = document.createElement("img");
+gif.src = "./images/smugtola-dancing.gif";
+gif.style.display = 'block';
+gif.style.marginLeft = "auto"
+gif.style.marginRight = "auto"
+gif.style.width = "100px"
 
 // ----------------------------------------------->> Login credentials
 
-const arr = [
-    {username: 'Diluted'}
-]
+const user = { username: "dilutedpilk", password: "SBA_316" };
+localStorage.clear();
+localStorage.setItem(`account`, JSON.stringify(user))
+
+// ----------------------------------------------->> Random div
+
+const frag = document.createDocumentFragment();
+frag.appendChild(document.createElement('hr'));
+frag.appendChild(document.createTextNode('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'));
+frag.appendChild(document.createElement('hr'));
+frag.appendChild(document.createElement('div'));
+frag.lastElementChild.textContent = "Getting the requirements in";
 
 // ----------------------------------------------->> Event listeners
 
@@ -71,23 +96,45 @@ navBar.addEventListener('click', (e) => {
     if (e.target === login && login.classList != 'active') {
         login.classList.add('active')
         dropdown.style.top = "8%"
+        dropdown.lastElementChild.style.top = "16%"
     } else if (e.target !== login && login.classList == 'active') {
         login.classList.remove('active');
         dropdown.style.top = "0"
+        dropdown.lastElementChild.style.top = "0"
     } else if (e.target === login && login.classList == 'active') {
         login.classList.remove('active');
-        dropdown.style.top = "0"
+        dropdown.style.top = "0";
+        dropdown.lastElementChild.style.top = "0";
     }
-})
+});
 
 main.addEventListener('click', (e) => {
     e.preventDefault();
     if (e.target !== login && login.classList == 'active') {
         login.classList.remove('active');
-        dropdown.style.top = "0"
+        dropdown.style.top = "0";
+        dropdown.lastElementChild.style.top = "0";
     }
-})
+});
 
-dropdown.addEventListener('submit', (e) =>{
+dropdown.addEventListener('click', (e) => {
 
-})
+    const username = dropdown.firstElementChild;
+    const pass = username.nextElementSibling;
+    const value = JSON.parse(localStorage.getItem("account"));
+    const submitButton = dropdown.lastElementChild;
+    
+    if (e.target === submitButton) { 
+        if (username.value.toLowerCase() === value.username && pass.value === value.password){
+            alert("Success!");
+            content.firstElementChild.textContent = "You are logged in.";
+            content.lastElementChild.textContent = "You can now see the rest of the page!";
+            content.appendChild(gif);
+            content.appendChild(frag);
+
+        } else {
+            alert("Username and/or password are incorrect. Please try again.");
+        }
+    }
+
+});
